3fc8dd6a4ef0c30c385a8dbf32cd9d52c997f66e
istiakAHAMMEDsaad <istik2020@gmail.com>
2026-07-23 23:44:50 +0600

    cors

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -87,15 +87,13 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { userId: user.id, role: user.role },
       ENV.JWT_SECRET,
-      {
-        expiresIn: "1d",
-      },
+      { expiresIn: "1d" },
     );
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -123,7 +121,7 @@ export const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "none",
   });
   return res
     .status(200)
@@ -143,6 +141,7 @@ export const getUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found." });
     }
+
     const { password, ...safeUser } = user;
 
     return res.status(200).json({ success: true, user: safeUser });
